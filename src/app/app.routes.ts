e171eefb6ab16c12277eaa9038b3f6998ceef4a6
nikolaossarris <nikolaossarrisnode@gmail.com>
2025-01-07 01:00:05 +0200

    the sixth commit full component (header) layout with ::after :hover , ticket , contact form buttons and nav links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,33 @@ import {LoginComponent} from './mainAppComp/login/login.component';
 import {RegisterComponent} from './mainAppComp/register/register.component';
 import {AuthGuard} from './auth.guard';
 import {PasswordRecoveryComponent} from './mainAppComp/password-recovery/password-recovery.component';
+import {MoviesComponent} from './mainAppComp/full/movies/movies.component';
+import {RoomsComponent} from './mainAppComp/full/rooms/rooms.component';
+import {ContactComponent} from './mainAppComp/full/contact/contact.component';
+import {HomeComponent} from './mainAppComp/full/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: FullComponent,
     canActivate: [AuthGuard],
-    children: []
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'movies',
+        component: MoviesComponent,
+        canActivate: [AuthGuard],
+      },
+      {
+        path: 'rooms',
+        component: RoomsComponent,
+        canActivate: [AuthGuard],
+      },
+    ]
   },
   {
     path: 'login',
--- src/app/mainAppComp/full/full.component.ts
+++ src/app/mainAppComp/full/full.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Tab, TabList, TabPanel, TabPanels, Tabs} from 'primeng/tabs';
+import {NgForOf} from '@angular/common';
+import {ActivatedRoute, Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {AuthenticationService} from '../../auth.service';
+import {MoviesComponent} from './movies/movies.component';
+import {RoomsComponent} from './rooms/rooms.component';
+import {Button, ButtonDirective} from 'primeng/button';
 
 @Component({
   selector: 'app-full',
   standalone: true,
-  imports: [],
+  imports: [
+    Tabs,
+    TabList,
+    Tab,
+    NgForOf,
+    RouterLink,
+    RouterOutlet,
+    TabPanels,
+    TabPanel,
+    MoviesComponent,
+    RoomsComponent,
+    ButtonDirective,
+    Button,
+    RouterLinkActive
+  ],
   templateUrl: './full.component.html',
   styleUrl: './full.component.css'
 })
-export class FullComponent {
+export class FullComponent implements OnInit{
 
+  currentUser: any;
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private authenticationService: AuthenticationService) {
+  }
+
+  ngOnInit() {
+    this.currentUser = this.authenticationService.currentUser();
+  }
+
+  refreshFull(){
+    this.router.navigate(['']);
+  }
+
+  toMovies(){
+    this.router.navigate(['movies']);
+  }
+
+  toRooms(){
+    this.router.navigate(['rooms']);
+  }
+
+  toContactForm(){
+    this.router.navigate(['contact']);
+  }
 }
